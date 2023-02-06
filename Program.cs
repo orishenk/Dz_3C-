31986be129ce9090a017e1a9a04fef952523bcea
Denis <stalininnokentij59@gmail.com>
2023-02-06 19:29:58 +0300

    DZ

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.
+/*
+
+void CheckingNumber(string number)
+{
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+}
+
+Console.WriteLine("введите пятизначное чимло:  ");
+string number = Console.ReadLine();
+CheckingNumber(number);
+*/
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+/*
+void CubeNum (int N)
+{
+int i = 1;
+while (i <= N)
+{
+    Console.Write(i * i * i + " ");
+    i++;
+}
+}
+Console.WriteLine("input a number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+CubeNum ( N);
+*/
